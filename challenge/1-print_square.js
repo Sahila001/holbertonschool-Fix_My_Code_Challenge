da863fbd65b8feee68ed6654536d2e42cea5cc03
Sahila001 <sahilamikayilova001@gmail.com>
2026-06-13 20:37:09 +0400

    .

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,14 +1,23 @@
 #!/usr/bin/node
+/*
+    Print a square with the character #
 
-function printSquare(size) {
-    if (isNaN(size) || size <= 0) {
-        return;
-    }
+    The size of the square must be the first argument
+    of the program.
+*/
 
-    for (let i = 0; i < size; i++) {
-        console.log('#'.repeat(size));
-    }
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1);
 }
 
 const size = parseInt(process.argv[2], 10);
-printSquare(size);
+
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
+}
