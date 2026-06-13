039009ae64a3de420fdad078ca58e336e09c6b4a
Sahila001 <sahilamikayilova001@gmail.com>
2026-06-13 20:54:59 +0400

    .

--- challenge/4-delete_dnodeint/print_dlistint.c
+++ challenge/4-delete_dnodeint/print_dlistint.c
@@ -10,12 +10,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t n;
+    size_t n = 0;
 
-	n = 0;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		n++;
-	}
+    while (h)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        n++;
+    }
+
+    return (n);
+}
